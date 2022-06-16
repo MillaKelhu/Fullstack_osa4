c544194958c2b4709130e5ba7f75f9b2b5b855ac
Milla Kelhu <milla.kelhu@gmail.com>
2022-06-16 07:26:45 +0300

    Move routes to a separate module

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Tämä tulee olemaan pelkkä käynnistystiedosto!
+
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -6,28 +8,12 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
