f767d9779787c8a4418140627fb18b889de2cf59
Milla Kelhu <milla.kelhu@gmail.com>
2022-06-16 07:41:58 +0300

    Moove db-connection to app module

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+    .then(result => {
+        logger.info('Connected to MongoDB, yay you')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB', error.message)
+    })
+
 const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
--- models/blog.js
+++ models/blog.js
@@ -4,15 +4,6 @@ const logger = require('../utils/logger')
 const config = require('../utils/config')
 const mongoose = require('mongoose')
 
-logger.info('connecting to', config.MONGODB_URI)
-mongoose.connect(config.MONGODB_URI)
-    .then(result => {
-        logger.info('Connected to MongoDB, yay you')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB', error.message)
-    })
-
 const blogSchema = mongoose.Schema({
     title: String,
     author: String,
