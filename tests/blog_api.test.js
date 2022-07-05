7b3b607ce9384459b8ed9ebcd74866fd6bc37e1b
Milla Kelhu <milla.kelhu@gmail.com>
2022-07-05 15:12:57 +0300

    Test 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -11,22 +11,39 @@ beforeEach(async () => {
     await Blog.insertMany(helper.initialBlogs)
 })
 
-describe('blogs', () => {
-    test('are returned as json', async () => {
-        await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-    })
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
 
-    test('have an identifying field id', async () => {
-        const blogs = await helper.blogsInDb()
-        const ids = blogs.map(b => b.id)
+test('blogs have an identifying field id', async () => {
+    const blogs = await helper.blogsInDb()
+    const ids = blogs.map(b => b.id)
+    for (const id of ids) {
+        expect(id).toBeDefined()
+    }
+})
 
-        for (const id of ids) {
-            expect(id).toBeDefined()
-        }
-    })
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        "title": "Bond and Banter",
+        "author": "Jack Lugo",
+        "url": "https://bondandbanter.libsyn.com/"
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAfterAddition = await helper.blogsInDb()
+    expect(blogsAfterAddition).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAfterAddition.map(b => b.title)
+    expect(titles).toContain(newBlog.title)
 })
 
 afterAll(() => {
