638a605dd9ba6c7563b6f0756efdc70c58c3ca69
Milla Kelhu <milla.kelhu@gmail.com>
2022-07-05 14:23:50 +0300

    Exercise 4.8

---
+++ tests/test_helper.js
@@ -0,0 +1,28 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+      "title": "License To Queer",
+      "author": "David Lowbridge-Ellis",
+      "url": "https://www.licencetoqueer.com/"
+    },
+    {
+      "title": "James Bond food",
+      "author": "Edward Biddulph",
+      "url": "https://jamesbondfood.com/"
+    },
+    {
+      "title": "BAMF Style",
+      "author": "Nick Guzan",
+      "url": "https://bamfstyle.com/"
+    },
+    {
+      "title": "007 Travelers",
+      "author": "Pirita & Mika",
+      "url": "https://007travelers.blogspot.com/"
+    }
+]
+
+module.exports = {
+    initialBlogs
+}
